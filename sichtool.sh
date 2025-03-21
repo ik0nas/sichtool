b1db3d4d56a50e7a8887353cc59e0c69a9b80f08
ik0nas <97743155+ik0nas@users.noreply.github.com>
2025-03-21 14:53:03 +0100

    Add files via upload

--- sichtool.sh
+++ sichtool.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
-echo -e "\nFühre das Skript mit "\$_" ./sicherungstool.sh www.domainname.tld im Terminal aus oder ohne der www-Subdomain mit "\$_" ./sicherungstool.sh domainname.tld, \nwenn die Sicherung zu klein ist.\n"
-echo -e "Beispiel 1: \$_ sicherungstool.sh www.domainname.com \nBeispiel 2: \$_ sicheurngstool.sh domainname.com \n\n"
+echo -e "\nFühre das Skript mit Terminal: ./webseitensicherung.sh www.domainname.tld im Terminal aus oder ohne der www-Subdomain mit Terminal: ./webseitensicherung.sh domainname.tld, \nwenn die Sicherung zu klein ist.\n"
+echo -e "Beispiel 1: \$_ ./webseitensicherung.sh www.domainname.com \nBeispiel 2: \$_ ./webseitensicherung.sh domainname.com \n\n"
 
 if [ "$#" -ne 1 ]; then
     echo -e "Es wurde noch kein Domainname bei der Ausführung benannt!\n"
-    echo -e "Bitte jetzt eingeben:\n"
+    echo -e "Bitte den Domainnamen jetzt eingeben:\n"
 	read -r domain
-    LOG_FILE=sicherungsprotokoll_"$domain".txt
-    echo -e "\nEs wird $domain gesichert.\n\n\n"
+    
+    NAME="$domain"
     starttime=$(date +%s)
+    time=$(date -d@"$starttime" -u +%H%M%S_%Y%m%d)
+    LOG="${NAME}-${time}-UTC"
+    mkdir -p ./"${LOG}"
+    echo -e "\nEs wird gesichert: https://$domain\n\n"
 
-    wget --restrict-file-names=windows --limit-rate=2000k --convert-links --random-wait -r -p -E -e robots=off -U mozilla https://"$domain"/ -q --show-progress --progress=bar:force:noscroll 2>&1 | tee "${LOG_FILE}"
-    echo -e "\n\n\n" | tee "${LOG_FILE}"
+    wget --restrict-file-names=windows --limit-rate=2000k --convert-links --random-wait -r -p -E -e robots=off -U mozilla https://"$domain"/ -P "./${LOG}/Sicherung-${time}-UTC" 2>&1 | tee "./${LOG}/Downloadlog_${LOG}.txt" 
+    
+    echo -e "\n"
     took=$(($(date +%s)-starttime))
     dauer=$(date -d@"$took" -u +%H:%M:%S)
-    echo -e "\t\tHeruntergeladen in $took Sekunden\n\t\tStunden:Minuten:Sekunden\t$dauer\n" | tee "${LOG_FILE}_dauer"
-    echo -e "Sicherung der Domain " "$domain" " ist abgeschlossen. Bitte manuell prüfen!"
-
+    echo -e "\t\tHeruntergeladen in $took Sekunden\n\t\tStunden:Minuten:Sekunden\t$dauer" | tee "./${LOG}/Ausführungsdauer-in-StundenMinutenSekunden_${dauer}.txt"
+    echo -e "\nSicherung der Domain https://${NAME} ist abgeschlossen. Bitte manuell prüfen!\n"
 else
-    LOG_FILE=logs_"$1".txt
-    echo -e "Es wird " "$1" " gesichert.\n\n\n" | tee "${LOG_FILE}1"
+    NAME="$1"
     starttime=$(date +%s)
+    time=$(date -d@"$starttime" -u +%H%M%S_%Y%m%d)
+    LOG="${NAME}-${time}-UTC"
+    mkdir -p ./"${LOG}"
+    echo -e "Es wird gesichert: https://"$1"\n\n"
 
-    wget --restrict-file-names=windows --limit-rate=2000k --convert-links --random-wait -r -p -E -e robots=off -U mozilla https://"$1"/ 2>&1 | tee "${LOG_FILE}2" #-q --show-progress --progress=bar:force:noscroll
-    echo -e "\n\n\n" | tee "${LOG_FILE}3"
+    wget --restrict-file-names=windows --limit-rate=2000k --convert-links --random-wait -r -p -E -e robots=off -U mozilla https://"$1"/ -P "./${LOG}/Sicherung-${time}-UTC" 2>&1 | tee "./${LOG}/Downloadlog_${LOG}.txt" 
+    
+    echo -e "\n"
     took=$(($(date +%s)-starttime))
     dauer=$(date -d@"$took" -u +%H:%M:%S)
-    echo -e "\t\tHeruntergeladen in $took Sekunden\n\t\tStunden:Minuten:Sekunden\t$dauer\n" | tee "${LOG_FILE}4"
-    echo -e "Sicherung der Domain " "$1" " ist abgeschlossen. Bitte manuell prüfen!" | tee "${LOG_FILE}5"
-
+    echo -e "\t\tHeruntergeladen in $took Sekunden\n\t\tStunden:Minuten:Sekunden\t$dauer" | tee "./${LOG}/Ausführungsdauer-in-StundenMinutenSekunden_${dauer}.txt"
+    echo -e "\nSicherung der Domain https://${NAME} ist abgeschlossen. Bitte manuell prüfen!\n"
 fi
-echo -e "\n\n"
